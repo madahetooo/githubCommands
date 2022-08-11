3b62264453b7bb626897c77ae7c8d5051ca2a4f1
madahetooo <ieslammedhat@gmail.com>
2022-08-11 17:21:28 +0200

    maybe conflict

--- app/src/main/java/com/apps/githubcomannds/MainActivity.kt
+++ app/src/main/java/com/apps/githubcomannds/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("This is master commit") // Master Branch
+        print("This is first Coadasdasdasfnflict Commit") // ChatFeature Branch
         print("This is first Conflict Commit") // ChatFeature Branch
     }
 }
