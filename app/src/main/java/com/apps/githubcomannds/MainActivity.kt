a7a57199fd7f5f5f74ba60d7375f96830925de8e
madahetooo <ieslammedhat@gmail.com>
2022-08-11 17:13:13 +0200

    commit again

--- app/src/main/java/com/apps/githubcomannds/MainActivity.kt
+++ app/src/main/java/com/apps/githubcomannds/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         print("This is master commit") // Master Branch
         print("This is first chatFeature Commit") // ChatFeature Branch
+        print("This is first Reels Commit") // ReelsFeature Branch
+
     }
 }
